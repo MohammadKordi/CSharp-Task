7d943fb4bcefbcf0ddb2fb53a2276d5068467bef
MohammadKordi <mohkordi21@gmail.com>
2024-10-03 19:27:15 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Q 1
+
+            //Console.WriteLine("Pleas inter Your Full Name \n");
+            //string FullName = Console.ReadLine();
+
+            //Console.WriteLine($"The Your Name : {FullName}\n") ;
+
+
+            // Q 2 
+
+            //string Name = "Mohammad Al Kordi";
+            //int Age = 20 ; 
+            //char Leter = 'M';
+            //double DoubleNumber = 123.36587;
+            //float FloatNumber = 143.76f; 
+            //bool Mared = false;
+            //const float PI = 3.14f;
+
+            //Console.WriteLine($" My Name is : {Name}\n Age : {Age}\n Leter : {Leter}\n Double Number : {DoubleNumber}\n Float Number : {FloatNumber}\n Mared : {Mared}\n Const Number : {PI} ");
+
+
+            // Q 3 
+
+            //string[] Cars = { "Camry", "Marceds", "BMW", "Ford" };
+            //Console.WriteLine(Cars[0] + " " + Cars[1] + " " + Cars[2] + " " + Cars[3] );
+
+
+            // Q 4 
+
+            //Console.Write("Inter your First Name: \n");
+            //string FirstName = Console.ReadLine();
+
+            //Console.Write("Inter your Last Name: \n");
+            //string LastName = Console.ReadLine();
+
+            //Console.Write("inter your Birthday: \n");
+            //string Birthday = Console.ReadLine();
+
+            //Console.WriteLine($"{FirstName} {LastName} {Birthday}");
+
+
+            // Q 5 
+
+            int[] Arry1 = { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine(Arry1[0] + " " + Arry1[1] + " " + Arry1[2] + " " + Arry1[3] + " " + Arry1[4] + " " + Arry1[5] + " " + Arry1[7] + " " + Arry1[8] + " " + Arry1[9]);
+
+            // Q 6
+
+            //int[] Arry2 = {8 , 5 , 2};
+            //Console.WriteLine((Arry2[0] + Arry2[1] + Arry2[2]));
+
+
+        }
+    }
+}
